0405b9c591b3a57624f3ca2b9bebd81a91b6c053
ScubaTron84 <16376969+ScubaTron84@users.noreply.github.com>
2020-06-27 22:47:44 -0400

    working nav mesh and player scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera camera;
+    public NavMeshAgent playerAgent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitPosition;
+            
+            //Check if mouse clicked something real
+            if(Physics.Raycast(ray, out hitPosition))
+            {
+                //move our agent
+                playerAgent.SetDestination(hitPosition.point);
+            }
+        }
+    }
+}
